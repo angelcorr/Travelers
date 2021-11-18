8aed68e3fd6f731bcc76c99da1baea2364513949
angelcorr <angelesemilianaa@gmail.com>
2021-11-18 10:38:02 -0500

    HTML & CSS of all pages. Social Network Travelers first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './my-components/body/body.component';
+import { HeaderComponent } from './my-components/header/header.component';
+import { SignUpComponent } from './my-components/sign-up/sign-up.component';
+import { TuristicPlaceProfileComponent } from './my-components/turistic-place-profile/turistic-place-profile';
+import { UserProfileComponent } from './my-components/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    SignUpComponent,
+    UserProfileComponent,
+    TuristicPlaceProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-components/turistic-place-profile/turistic-place-profile.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-turistic-place-profile',
+  templateUrl: './turistic-place-profile.html',
+  styleUrls: ['./turistic-place-profile.css']
+})
+export class TuristicPlaceProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
